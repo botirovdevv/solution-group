560a9f5bd3bf691bb849ddd3f8acb6c2a93080aa
botirovdev <botirovdev7@gmail.com>
2024-07-14 21:34:16 +0500

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/header/Navbar'
 import Header from './components/header/Header'
 import Service from './components/main/Service'
@@ -6,17 +6,35 @@ import Projects from './components/main/Projects'
 import Contact from './components/main/Contact'
 import About from './components/main/About'
 import Footer from './components/footer/Footer'
+import { PulseLoader } from 'react-spinners'
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3200);
+  }, []);
   return (
     <main>
-      <Navbar/>
-      <Header/>
-      <About/>
-      <Service/>
-      <Projects/>
-      <Contact/>
-      <Footer/>
+      {
+        loading ? (
+          <div className='loader'>
+            <PulseLoader color='rgb(38, 63, 96)' size={20} />
+          </div>
+        ) : (
+          <div className="wrapper">
+            <Navbar />
+            <Header />
+            <About />
+            <Service />
+            <Projects />
+            <Contact />
+            <Footer />
+          </div>
+        )
+      }
     </main>
   )
 }
